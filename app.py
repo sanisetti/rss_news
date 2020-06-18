c2beb701778ba7ca07c6394ce7c23b8328f53764
Sathya Anisetti <sathyaanisetti@Sathyas-MacBook-Pro.local>
2020-06-18 11:21:30 -0400

    added isa

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
 from bs4 import BeautifulSoup
 import urllib.request
+import json
 
 app = Flask(__name__)
 journalRSSMap = {'wsj': 'https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml',
@@ -8,7 +9,11 @@ journalRSSMap = {'wsj': 'https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml',
                  'mit': 'https://www.technologyreview.com/feed/',
                  'econ': 'https://www.economist.com/finance-and-economics/rss.xml',
                  'mck': 'https://www.mckinsey.com/insights/rss.aspx',
-                 'nyt': 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'}
+                 'nyt': 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+                 'lat': 'https://www.latimes.com/news/rss2.0.xml',
+                 'buzz': 'https://www.buzzfeed.com/index.xml',
+                 'cos': 'https://www.cosmopolitan.com/rss/all.xml/',
+                 'npr': 'https://feeds.npr.org/1019/feed.json'}
 
 @app.route('/news/<journal>')
 def getArticles(journal):
@@ -19,12 +24,21 @@ def getArticles(journal):
         soup = BeautifulSoup(page, "html")
         for entry in soup.find_all('entry'):
             toReturn.append(entry.link['href'])
+    elif journal == 'npr':
+        page = urllib.request.urlopen(url)
+        data = json.loads(page.read())['items']
+        for obj in data:
+            toReturn.append(obj['url']))
     else:
         page = urllib.request.urlopen(url)
         soup = BeautifulSoup(page, features="xml")
         for item in soup.find_all('item'):
             toReturn.append(item.link.text)
-    return jsonify(toReturn);
+    return jsonify(toReturn)
+
+@app.route('/isa')
+def isa():
+    return render_template('isa.html')
 
 @app.route('/')
 def home():
